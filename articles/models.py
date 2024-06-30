175c55deb693a2e3bb50269ef3d21986ad621249
Tracy To <tracyto101@gmail.com>
2024-06-29 23:08:55 -0400

    updated model again (4th time)

--- articles/models.py
+++ articles/models.py
@@ -16,6 +16,14 @@ class Article(models.Model):
   media = models.URLField(blank=True, null=True)
   category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, blank=False)
   author = models.CharField(max_length=100, blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.title
+  
+  def formatted_date(self):
+        return self.created_at.strftime('%B %d, %Y')
+  
+  def formatted_updated_date(self):
+        return self.updated_at.strftime('%B %d, %Y')
